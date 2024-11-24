915da2892abd7deb83362c3fe5953865ac89310f
Augusto Pasquali de Lima <augusto.pasqualidelima@gmail.com>
2024-11-24 20:54:23 -0300

    Adicionando validador de número de telefone

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -3,6 +3,8 @@ package helpers
 import (
 	"net/mail"
 	"net/url"
+	"strings"
+	"unicode"
 )
 
 func ValidateEmail(email string) bool {
@@ -22,4 +24,23 @@ func ValidateUrl(address string) bool {
 	}
 
 	return err == nil
+}
+
+func ValidateCellphoneNumber(cellphoneNumber string) bool {
+	cellphoneNumber = strings.ReplaceAll(cellphoneNumber, " ", "")
+	cellphoneNumber = strings.ReplaceAll(cellphoneNumber, "-", "")
+	cellphoneNumber = strings.ReplaceAll(cellphoneNumber, "(", "")
+	cellphoneNumber = strings.ReplaceAll(cellphoneNumber, ")", "")
+
+	// Verifica se o número contém apenas dígitos
+	for _, char := range cellphoneNumber {
+		if !unicode.IsDigit(char) {
+			return false
+		}
+	}
+
+	// Verifica se o número tem tamanho válido (11 dígitos)
+	length := len(cellphoneNumber)
+
+	return length == 11
 }
